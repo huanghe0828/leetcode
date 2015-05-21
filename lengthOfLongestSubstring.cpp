7a822ef700d170a9571570e508ff393ea7a2e474
huanghe0828 <huanghe0828@gmail.com>
2015-05-20 19:59:02 -0700

    Create lengthOfLongestSubstring.cpp

---
+++ lengthOfLongestSubstring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char, int> charMap;
+        int maxLen = 0;
+        int cLen = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (charMap.find(c) != charMap.end()) cLen = min(cLen, i-charMap[c]-1);
+            cLen++;
+            charMap[c] = i;
+            maxLen = max(maxLen, cLen);
+        }
+        return maxLen;
+    }
+};
